88647c7e32b52765186baae133729c79ae6295b0
Fernando Branbila <fernandobjunior@sas.com.br>
2021-08-22 18:47:14 -0300

    validate instructions

--- src/main/java/br/com/mars/exploringmars/domain/planet/usecase/ExecuteRoverInstructions.java
+++ src/main/java/br/com/mars/exploringmars/domain/planet/usecase/ExecuteRoverInstructions.java
@@ -1,5 +1,6 @@
 package br.com.mars.exploringmars.domain.planet.usecase;
 
+import br.com.mars.exploringmars.domain.exception.UnprocessableEntityException;
 import br.com.mars.exploringmars.domain.planet.gateway.inbound.MoveRoverInbound;
 import br.com.mars.exploringmars.domain.planet.model.Rover;
 import br.com.mars.exploringmars.infastructure.planet.gateway.FindPlateauByIdProvider;
@@ -25,23 +26,26 @@ public class ExecuteRoverInstructions implements MoveRoverInbound {
 
     @Override
     public Rover execute(Long planetId, Long plateauId, Long roverId, String roverInstructions) {
-        validateInstructions(); //regex LRM
+        validateInstructions(roverInstructions);
         var plateau = findPlateauByIdProvider.execute(plateauId);
-        var rover = findRoverByIdProvider.execute(roverId);
         for (char ch : roverInstructions.toCharArray()) {
+            var rover = findRoverByIdProvider.execute(roverId);
             executeRoverInstructionsStrategyList.stream()
                     .filter(strategy -> strategy.filterInstruction(ch, rover.getFacingSide()))
                     .findFirst()
                     .ifPresent(strategy ->
                             strategy.execute(
                                     plateau,
-                                    findRoverByIdProvider.execute(roverId),
+                                    rover,
                                     ch
                             )
                     );
         }
-
         return rover;
+    }
 
+    private void validateInstructions(String roverInstructions) {
+        if (!roverInstructions.replaceAll("[MLR]", "").isEmpty())
+            throw new UnprocessableEntityException("Invalid rover instructions input");
     }
 }
